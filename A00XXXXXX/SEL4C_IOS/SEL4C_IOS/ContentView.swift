802834179500fb07f935a11012330bdd9797fdb7
Ivan Santiago Mendez <ivan.santiago@tec.mx>
2023-08-22 18:58:44 -0600

    Adding openAPI

---
+++ A00XXXXXX/SEL4C_IOS/SEL4C_IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SEL4C_IOS
+//
+//  Created by Ivan Santiago Méndez on 21/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
